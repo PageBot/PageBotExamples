05322028597704990a55006dbdd723ec5e66b011
petrvanblokland <buro@petr.com>
2019-06-21 07:49:27 -0400

    Working on examples

---
+++ Publications/Typespecimens/DemoTypeSpecimen.py
@@ -0,0 +1,28 @@
+
+from pagebot.toolbox.units import *
+from pagebot.toolbox.color import *
+from pagebot.document import Document
+from pagebot.constants import *
+from pagebot.elements import *
+from pagebot.conditions import *
+
+H, W = A4
+doc = Document(w=W, h=H, originTop=False, autoPages=1)
+view = doc.view
+view.padding = inch(1)
+view.showFrame = True
+view.showRegistrationMarks = True
+view.showCropMarks = True
+view.showColorBars = True
+
+page = doc[1]
+page.padding = p(6)
+page.showPadding = True
+page.bleed = mm(3)
+
+newRect(parent=page, fill=color('red'),
+w=page.w*0.6+page.bleedLeft, conditions=[Left2LeftBleed(), Top2TopSide()])
+newRect(parent=page, fill=color('blue'), mt=20, conditions=[Left2Left(), Float2Top()])
+
+print(page.solve())
+doc.export('_export/OurTypeSpeciment.pdf')
