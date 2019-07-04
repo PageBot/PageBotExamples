2ba168fa471c73ca68bfcd223a244b35949eb7de
petrvanblokland <buro@petr.com>
2019-07-04 07:45:05 +0200

    De-organizing and testing Publication classes

--- Publications/Books/MakeABookCover.py
+++ Publications/Books/MakeABookCover.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -----------------------------------------------------------------------------
 #     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
 #     www.pagebot.io
@@ -7,26 +7,28 @@
 #
 #     Licensed under MIT conditions
 #
-#     Supporting usage of DrawBot, www.drawbot.com
-#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
 #     MakeABookCover.py
 #
 #     Needs filling in with content.
 #
-from pagebot.contexts import defaultContext as context
 from random import random # Used for random color palet.
 
 # Create random title and names
 from pagebot.contributions.filibuster.blurb import blurb
+from pagebot.toolbox.color import color, noColor, whiteColor
 
-from pagebot.toolbox.transformer import darker
-# Get function to find the Roboto family (in this case installed in the PageBot repository
+# Get function to find the Roboto family (in this case installed in the
+# PageBot repository).
 from pagebot.fonttoolbox.objects.family import getFamily
+from pagebot.fonttoolbox.objects.font import findFont
 # Creation of the RootStyle (dictionary) with all
 # available default style parameters filled.
-from pagebot.style import getRootStyle, B4, CENTER, MIDDLE, TOP 
+from pagebot.style import getRootStyle
+from pagebot.constants import B4, CENTER, MIDDLE, TOP
 
 # Document is the main instance holding all information
 # about the document togethers (pages, styles, etc.)
@@ -35,14 +37,17 @@ from pagebot.document import Document
 # Import element layout conditions.
 from pagebot.conditions import *
 from pagebot.elements import newRect, newTextBox
-   
+from pagebot.toolbox.units import em
+
 # For clarity, most of the MakeABookCover.py example document is setup
 # as a sequential excecution of Python functions. For complex documents
 # this is not the best method. More functions and classes will be used in the
 # real templates, which are available from the PageBotTemplates repository.
 
 W, H = B4
-W -= 48 # Make a bit more narrow format.
+#W -= 48 # Make a bit more narrow format.
+
+BLEED = 8
 
 # Export in folder that does not commit to Git. Force to export PDF.
 EXPORT_PATH = '_export/ABookCover.pdf'
@@ -51,19 +56,20 @@ family = getFamily('Roboto')
 fontRegular = family.findFont('Regular')
 fontBold = family.findFont('Bold')
 fontItalic = family.findFont('Italic')
+ampersandFont = findFont('Georgia')
 
 def makeDocument():
-    u"""Demo random book cover generator."""
+    """Demo random book cover generator."""
 
     # Create new document with (w,h) and fixed amount of pages.
     # Make number of pages with default document size.
     # Initially make all pages default with template
-    doc = Document(w=W, h=H, title='A Demo Book Cover', autoPages=1, context=context,
-        originTop=False) # One page, just the cover.
+    # One page, just the cover.
+    doc = Document(w=W, h=H, title='A Demo Book Cover', autoPages=1, originTop=False)
 
     page = doc[1] # Get the first/single page of the document.
     page.name = 'Cover'
-    
+
     # Get the current view of the document. This allows setting of
     # parameters how the document is represented on output.
     view = doc.view
@@ -71,14 +77,16 @@ def makeDocument():
     # Set view options. Full list is in elements/views/baseviews.py
     view.padding = 40 # Showing cropmarks and registration marks
                       # need >= 20 padding of the view.
-    view.showPageRegistrationMarks = True
-    view.showPageCropMarks = True
-    view.showPageFrame = True
-    view.showPagePadding = False
-    view.showPageNameInfo = True
+    view.showRegistrationMarks = True
+    view.showCropMarks = True
+    view.showFrame = True
+    view.showPadding = False
+    view.showNameInfo = True
     view.showTextOverflowMarker = False
-    
-    C1 = (random()*0.2, random()*0.2, random()*0.9)
+
+    context = view.context
+
+    C1 = color(r=random()*0.2, g=random()*0.2, b=random()*0.9)
 
     # Make background element, filling the page color and bleed.
     colorRect1 = newRect(z=-10, name='Page area', parent=page,
@@ -87,16 +95,16 @@ def makeDocument():
                                      Fit2RightSide(),
                                      Fit2BottomSide()],
                          fill=C1)
-    colorRect1.bleed = 0 #(0, 0, 0, 40) # TODO: Fix Bleed function
+    colorRect1.bleed = BLEED
     colorRect1.solve() # Solve element position, before we can make
                        # other elements depend on position and size.
 
-    M = 64
-    newRect(z=-10, name='Frame 2', parent=colorRect1, 
+    M = BLEED + 64
+    colorRect2 = newRect(z=-10, name='Frame 2', parent=colorRect1,
             conditions=[Center2Center(), Middle2Middle()],
-            fill=darker(C1, 0.5), # Default parameter:
+            fill=C1.darker(0.5), # Default parameter:
                                   # 50% between background color and white
-            stroke=None,
+            stroke=noColor,
             w=colorRect1.w-M, h=colorRect1.h-M,
             xAlign=CENTER, yAlign=MIDDLE)
 
@@ -108,31 +116,34 @@ def makeDocument():
     authorName = blurb.getBlurb('name', noTags=True)
     if random() < 0.33: # 1/3 chance for a second author name
         authorName += '\n' + blurb.getBlurb('name')
-        
-    page.pt = 120 # Now the rectangles positioned automatic, alter the paddings
-    page.pl = page.pr = 80
-    page.pb = 30
+
+    titleStyle = dict(font=fontBold.path, fontSize=40, leading=em(1.2),
+        xTextAlign=CENTER, textFill=whiteColor, hyphenation=False)
+    subTitleStyle = dict(font=fontRegular.path, fontSize=32,
+        xTextAlign=CENTER, textFill=(1, 1, 1, 0.5), hyphenation=False)
+    authorStyle = dict(font=fontItalic.path, fontSize=24, tracking=em(0.025),
+        xTextAlign=CENTER, textFill=(1, 0.5, 1,0.7))
+
     # Add some title (same width, different height) at the "wrongOrigin" position.
     # They will be repositioned by solving the colorConditions.
-    title = context.newString(title+'\n\n', style=dict(font=fontBold.path, fontSize=40, rLeading=1.2, xAlign=CENTER, textFill=1))
-    title += context.newString(subTitle + '\n\n', style=dict(font=fontRegular.path, fontSize=32, xAlign=CENTER, textFill=(1, 1, 1,0.5)))
-    title += context.newString(authorName, style=dict(font=fontItalic.path, fontSize=24, rTracking=0.025, xAlign=CENTER, textFill=(1, 0.5, 1,0.7)))
-    newTextBox(title, parent=page, name='Other element',
+    title = context.newString(title+'\n\n', style=titleStyle)
+    title += context.newString(subTitle + '\n\n', style=subTitleStyle)
+    title += context.newString(authorName, style=authorStyle)
+    newTextBox(title, parent=colorRect2, name='Other element',
             conditions=[Fit2Width(), Center2Center(), Top2Top()],
             xAlign=CENTER, yAlign=TOP)
-    
-    typoIllustration = context.newString('&', style=dict(font='Georgia', fontSize=400, xAlign=CENTER, textFill=(1, 0.5, 1,0.7)))
+
+    typoIllustration = context.newString('&', style=dict(font=ampersandFont.path, fontSize=300, xTextAlign=CENTER, textFill=(1, 0.5, 1,0.7)))
     newTextBox(typoIllustration, parent=page,
             conditions=[Fit2Width(), Center2Center(), Bottom2Bottom()],
             xAlign=CENTER, yAlign=TOP)
 
+    # Evaluate again, result should now be >= 0
     score = page.evaluate()
-    if score.fails:
+    if score.fails: # There is new "failing" elements. Solve their layout.
         page.solve()
 
-    # Evaluate again, result should now be >= 0
     return doc
 
 d = makeDocument()
 d.export(EXPORT_PATH)
-
