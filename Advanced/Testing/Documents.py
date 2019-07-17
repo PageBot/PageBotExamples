b1bd22b83e4eaec850b4e25cde0356efa46d53b3
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-07-17 11:15:04 +0200

    Switching to sequential tests of objects.

--- Advanced/Testing/Documents.py
+++ Advanced/Testing/Documents.py
@@ -12,7 +12,7 @@
 #
 #     Documents.py
 #
-#     Tests pagebot string and style classes in different contexts.
+#     Pagebot documents tests. Should become unit tests when finished.
 
 from pagebotcocoa.contexts.drawbot.context import DrawBotContext
 from pagebot.contexts.flat.context import FlatContext
@@ -55,30 +55,52 @@ testContexts = (
     #(IdmlContext(), '_export/testIdmlString.pdf')
 )
 
-def testContext(context, path):
+def testDocument(context):
     doc = Document(w=W, h=H, context=context)
+    print('# Document in %s is %s' % (context, doc))
+    return doc
+
+def testPages(doc):
     page = doc[1]
-    #print('Current page: %s' % page)
+    print('Current page: %s' % page)
     nextPage = page.next
-    #print('Next page: %s' % nextPage)
-    #print(type(page))
-    #print('Units: %s' % context.units)
-    #print('# Testing document in %s' % context)
+    print('Next page: %s' % nextPage)
+    print(type(page))
+    print(doc.pages)
+
+def testUnits(context):
+    print('Units: %s' % context.units)
+
+
+def testElements(page):
     conditions = [Right2Right(), Float2Top(), Float2Left()]
 
     for n in range(10):
         newLine(x=100, y=n*100, parent=page, stroke=0)
 
+    '''
     for n in range(10):
         newRect(w=40, h=42, mr=4, mt=4, parent=nextPage,
                 fill=color(random()*0.5 + 0.5, 0, 0.5),
                 conditions=conditions)
     score = nextPage.solve()
     #print(score)
+    '''
+
+def build(doc):
     doc.build() # Export?
 
-def testAllContexts():
+def test():
     for context, path in testContexts:
-        testContext(context, path)
+        doc = testDocument(context)
+        testUnits(context)
+        testPages(doc)
+        # TODO: maybe make this work?
+        #page = doc.pages[-1]
+        page = doc.pages[1]
+        print(page)
+        print(type(page))
+        #testElements(page)
+        build(doc)
 
-testAllContexts()
+test()
