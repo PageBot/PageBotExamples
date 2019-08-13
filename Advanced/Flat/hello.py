745e4e24634c4a2a678370897c05f5c53deda23c
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-08-13 10:32:57 +0200

    A5 sized output.

--- Advanced/Flat/hello.py
+++ Advanced/Flat/hello.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 
 from pagebot.fonttoolbox.objects.font import findFont
+from pagebot.constants import A5
+from pagebot.toolbox.units import pt
 from flat import rgb, font, shape, strike, document
+w, h = pt(A5)
+w = int(w)
+h = int(h)
 
-roboto = findFont('Bungee-Regular')
-print('bla')
-print(roboto)
+bungee = findFont('Bungee-Regular')
 red = rgb(255, 0, 0)
-print(roboto.path)
-lato = font.open(roboto.path)
+lato = font.open(bungee.path)
 figure = shape().stroke(red).width(2.5)
 headline = strike(lato).color(red).size(20, 24)
 
-d = document(100, 100, 'mm')
+d = document(w, h, 'pt')
 p = d.addpage()
 p.place(figure.circle(50, 50, 20))
 p.place(headline.text('Hello world!')).frame(10, 10, 80, 80)
