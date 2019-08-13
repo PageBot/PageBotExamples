da1a0e72ef811f0c07152e407ef95a3f2710f1fc
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-08-13 12:31:55 +0200

    Testing placed text overflow.

--- Advanced/Flat/hello.py
+++ Advanced/Flat/hello.py
@@ -5,6 +5,7 @@ from pagebot.constants import A5
 from pagebot.toolbox.units import pt
 from pagebot.contributions.filibuster.blurb import Blurb
 from flat import * # rgb, font, shape, strike, document
+import random
 
 w, h = pt(A5)
 w = int(w)
@@ -16,10 +17,20 @@ bungee = findFont('Bungee-Regular')
 bungeeFlat = font.open(bungee.path)
 
 blurb = Blurb()
-txt = blurb.getBlurb('book_pseudoscientific', noTags=True)
+types = blurb.getBlurbTypes()
 
+txts = []
+
+for i in range(5):
+    i = random.randint(0, len(types))
+    t = types[i]
+    print(t)
+    txts.append(blurb.getBlurb(t, noTags=True))
+
+txt = '. '.join(txts)
+print(txt)
 headline = strike(bungeeFlat).color(red).size(20, 24)
-body = strike(bungeeFlat).color(black).size(12, 14)
+body = strike(bungeeFlat).color(black).size(10, 12)
 
 redStroke = shape().stroke(red).width(0.5)
 blackFill = shape().nostroke().fill(black)
@@ -32,13 +43,15 @@ p.place(redStroke.circle(50, 50, 20))
 p.place(blackFill.rectangle(200, 200, 20, 40))
 
 p.place(headline.text('Hello world!')).frame(10, 10, 80, 80)
+
 w = 200
 h = 100
-x = 50
+x = 20
 y = 100
-p.place(body.text(txt)).frame(x, y, w, h)
-p.place(blackStroke.rectangle(x, y, w, h))
+placedText = p.place(body.text(txt)).frame(x, y, w, h)
+print(placedText.overflow())
 
+p.place(blackStroke.rectangle(x, y, w, h))
 
 # Export.
 p.image(kind='rgb').png('hello.png')
