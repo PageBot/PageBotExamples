8042635f354e15a246fa0190362aa4d7adae0646
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-08-14 14:22:25 +0200

    Calculate overflowing text to be printed on next page.

--- Advanced/Flat/layout.py
+++ Advanced/Flat/layout.py
@@ -27,7 +27,7 @@ blurb = Blurb()
 title = blurb.getBlurb('stylewars_original')
 txt = blurb.getBlurb('stylewars_documentary')
 headline = strike(bungeeFlat).color(red).size(20, 24)
-body = strike(bungeeFlat).color(black).size(10, 12)
+body = strike(bungeeFlat).color(white).size(10, 12)
 
 redStroke = shape().stroke(red).width(0.5)
 blackFill = shape().nostroke().fill(black)
@@ -40,7 +40,7 @@ p = d.addpage()
 p.place(redStroke.circle(50, 50, 20))
 padding = 10
 w0 = w - 2* padding
-h0 = 300
+h0 = h - 2 * padding
 x = padding
 y = padding
 
@@ -49,13 +49,41 @@ p.place(blackFill.rectangle(padding, padding, w0, 24))
 p.place(headline.text(title)).frame(padding, padding, w0, 24)
 y += 24
 
-p.place(blackStroke.rectangle(x, y, w0, h0))
-p.place(whiteFill.rectangle(x, y, w0, h0))
-placedText = p.place(body.text(txt)).frame(x, y, w0, h0)
-print(placedText.overflow())
+#p.place(blackStroke.rectangle(x, y, w0, h0))
+#p.place(whiteFill.rectangle(x, y, w0, h0))
+t0 = body.text(txt)
+strikeWidth = body.width(txt)
+print(strikeWidth/ w0)
+placedText = p.place(t0).frame(x, y, w0, h0 - y)
+
+of = placedText.overflow()
+lines = len(placedText.lines())
+print(lines)
+textHeight = (lines+1) * 12
+y += textHeight
+
+t2 = blurb.getBlurb('stylewars_bluray')
+placedText = p.place(body.text(t2)).frame(x, y, w0, h0 - y)
+import difflib
+
+if placedText.overflow():
+    usedText = ''.join(placedText.lines())
+    unusedText = ''
+
+
+    for i,s in enumerate(difflib.ndiff(t2, usedText)):
+        if s[0]==' ': continue
+        elif s[0]=='-':
+            unusedText += s[-1]
+            #print(u'Delete "{}" from position {}'.format(s[-1],i))
+        elif s[0]=='+':
+            print(u'Add "{}" to position {}'.format(s[-1],i))
+
+    print(unusedText)
 
 
 # Export.
-p.image(kind='rgb').png('%s.png' % documentName)
-p.svg('%s.svg' % documentName)
-d.pdf('%s.pdf' % documentName)
+export = '_export'
+p.image(kind='rgb').png('%s/%s.png' % (export, documentName))
+p.svg('%s/%s.svg' % (export, documentName))
+d.pdf('%s/%s.pdf' % (export, documentName))
