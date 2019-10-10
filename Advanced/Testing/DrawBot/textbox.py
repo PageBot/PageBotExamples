44572940876de8dff7eb1bf29e786691b7302751
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-10-10 14:12:23 +0200

    Testing overflow.

--- Advanced/Testing/DrawBot/textbox.py
+++ Advanced/Testing/DrawBot/textbox.py
@@ -17,3 +17,8 @@ hyphenation(True)
 
 tb = textBox("""The 25-storey Jumeirah Beach Hotel, with its distinctive design in the shape of a wave, has become one of the most successful hotels in the world.
 Located on Jumeirah Beach, this well-known hotel offers a wonderful holiday experience and a variety of pleasurable activities. The many restaurants, bars and cafés, daily live entertainment and sports facilities will keep you entertained, whilst children will have a great time at the Sinbad’s Kids’ Club or Wild Wadi WaterparkTM which is freely accessible through a private gate.""", box)
+
+# Overflow.
+print(type(tb))
+print(len(tb))
+print(tb)
