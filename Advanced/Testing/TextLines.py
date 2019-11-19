73223e55c040a3b713e3c4397ac5210087911a59
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-11-19 10:05:47 +0100

    Printing textline results.

--- Advanced/Testing/TextLines.py
+++ Advanced/Testing/TextLines.py
@@ -15,6 +15,8 @@
 
 from pagebot import getContext
 from pagebot.contributions.filibuster.blurb import Blurb
+from pagebot.toolbox.units import pt
+from pagebot.toolbox.color import color
 
 H = 850
 W = 652
@@ -23,13 +25,14 @@ blurb = Blurb()
 context = getContext('DrawBot')
 context.newDrawing()
 context.newPage(W, H)
-
 b = blurb.getBlurb('stylewars_documentary')
-s = context.newString(b)
-lines = s.getTextLines(w=100)
+style = dict(font='Helvetica', fontSize=pt(18), textFill=color(1, 1, 0))
+s = context.newString(b, style=style)
+lines = s.getTextLines(w=300, h=1200)
+
 for line in lines:
     for run in line.textRuns:
-        print(run)
+        print(run.string)
 
 context.text(s, (0, 0))
 
