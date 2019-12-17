88cfc58ca0ce81e70bb3fde964b11d8a094bddf8
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-12-17 12:23:21 +0100

    Testing AppKit leading, seems to always be zero.

--- Advanced/Testing/DrawBot/FormattedString.py
+++ Advanced/Testing/DrawBot/FormattedString.py
@@ -16,17 +16,17 @@ fontSize = 100
 lineHeight = leading * fontSize
 s = FormattedString('bla', font='Helvetica', fontSize=fontSize, lineHeight=lineHeight, fill=(0.4,0.5,0.2))
 
-'''
+
 fontSize = 300
-lineHeight = fontSize * leading
+#lineHeight = fontSize * leading
 s.fontSize(fontSize)
-s.lineHeight(lineHeight)
+s.lineHeight(lineHeight) # Keeps value of first part.
 s.font('Helvetica-Bold')
 s.fill(0.3, 0.2, 0.9)
 s += 'bla2'
-'''
 
-text(s, (0,0))
+
+text(s, (0,lineHeight))
 
 print('formatted string leading %s' % s.fontLeading())
 print('formatted string size %s' % s.size())
@@ -38,4 +38,8 @@ print(s.fontDescender())
 w, h = s.size()
 fill(None)
 stroke(0)
-rect(0, 0, w, h)
+rect(0, lineHeight, w, h)
+
+from AppKit import NSFont
+font = NSFont.fontWithName_size_("Helvetica", 12)
+print(font.leading())
