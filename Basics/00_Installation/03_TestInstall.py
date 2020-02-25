ee61ef8979c8accddfa1af868312d035ea65825f
Michiel <michiel@kauwatjoe.com>
2020-02-25 11:55:26 +0100

    Adding dependency tests.

---
+++ Basics/00_Installation/03_TestInstall.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
+# -----------------------------------------------------------------------------
+#
+#    03_TestInstall.py
+#
+
+# Python version.
+import sys
+print(sys.version)
+import platform
+print (platform.python_version())
+
+# Libary locations.
+import site
+print(site.getsitepackages())
+
+# Pagebot.
+import pagebot
+from pagebot.constants import DEFAULT_TRACKING
+print(pagebot)
+
+# Uncomment on Mac.
+#import pagebotcocoa
+#print(pagebotcocoa)
+
+import flat
+print(flat)
+
+import booleanOperations
+import fontTools
+import sass
+import markdown
+import svgwrite
+import tornado
+from simple_idml import idml
