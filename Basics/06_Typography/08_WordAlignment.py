faeedf202da1d32548d28f5f5c463eaaef381353
Michiel <michiel@kauwatjoe.com>
2020-03-09 13:48:39 +0100

    Testing word alignments.

--- Basics/06_Typography/08_WordAlignment.py
+++ Basics/06_Typography/08_WordAlignment.py
@@ -13,7 +13,7 @@
 #
 #     08-HeadlineAlignment.py
 #
-from pagebotcocoa.contexts.drawbot.drawbotcontext import DrawBotContext
+from pagebot import getContext
 from pagebot.fonttoolbox.objects.font import findFont
 from pagebot.toolbox.units import em, p, pt
 from pagebot.constants import *
@@ -22,53 +22,62 @@ from pagebot.conditions import *
 from pagebot.document import Document
 from pagebot.toolbox.color import color
 
-context = DrawBotContext()
+
 
 W, H = pt(800, 500) # Document size
 PADDING = pt(120, 20) # Page padding on all sides
 
 NUM_PAGES = 1
 
-# Get the font object, from te Roboto file that is included in PageBot resources for testing.
-f = findFont('PageBot-Regular')
+def wordAlignment(context):
+    # Get the font object, from te Roboto file that is included in PageBot resources for testing.
+    f = findFont('PageBot-Regular')
 
-# Create a new document with 1 page. Set overall size and padding.
-# TODO: View grid drawing, etc. does not work properly for originTop=True
-doc = Document(w=W, h=H, padding=PADDING, context=context, 
-    autoPages=NUM_PAGES, originTop=False)
-view = doc.view
-view.showPadding = True
+    # Create a new document with 1 page. Set overall size and padding.
+    # TODO: View grid drawing, etc. does not work properly for originTop=True
+    doc = Document(w=W, h=H, padding=PADDING, context=context,
+        autoPages=NUM_PAGES, originTop=False)
+    view = doc.view
+    view.showPadding = True
 
-page = doc[1]
-page.padding = PADDING
+    page = doc[1]
+    page.padding = PADDING
 
-t = 'Hkpx'
+    t = 'Hkpx'
 
-for c, conditions in (
-	('green', [ 
-	    Shrink2TextHeight(), Shrink2TextWidth(), 
-	    Left2Left(), CapHeight2Top()]),
-	('red', [ 
-	    Shrink2TextHeight(), Shrink2TextWidth(), 
-	    Center2Center(), Baseline2Top()]),
-	('blue', [ 
-	    Shrink2TextHeight(), Shrink2TextWidth(), 
-	    Right2Right(), XHeight2Top()]),
-	#('green', [ 
-	#    Shrink2TextHeight(), Shrink2TextWidth(), 
-	#    Left2Left(), CapHeight2Bottom()]),
-	('red', [ 
-	    Shrink2TextHeight(), Shrink2TextWidth(), 
-	    Center2Center(), Baseline2Bottom()]),
-	#('blue', [ 
-	#    Shrink2TextHeight(), Shrink2TextWidth(), 
-	#    Right2Right(), XHeight2Bottom()]),
+    for c, conditions in (
+            ('green', [
+                Shrink2TextHeight(), Shrink2TextWidth(),
+                Left2Left(), CapHeight2Top()]),
+            ('red', [
+                Shrink2TextHeight(), Shrink2TextWidth(),
+                Center2Center(), Baseline2Top()]),
+            ('blue', [
+                Shrink2TextHeight(), Shrink2TextWidth(),
+                Right2Right(), XHeight2Top()]),
+            #('green', [
+            #    Shrink2TextHeight(), Shrink2TextWidth(),
+            #    Left2Left(), CapHeight2Bottom()]),
+            ('red', [
+                Shrink2TextHeight(), Shrink2TextWidth(),
+                Center2Center(), Baseline2Bottom()]),
+            #('blue', [
+            #    Shrink2TextHeight(), Shrink2TextWidth(),
+            #    Right2Right(), XHeight2Bottom()]),
 
-):
-	style = dict(font=f, fontSize=100, textFill=color(name=c))
-	bs = context.newString(t, style=style)
-	newTextBox(bs, parent=page, fill=(0.5, 0.5, 0.5, 0.5), conditions=conditions)
+    ):
+            style = dict(font=f, fontSize=100, textFill=color(name=c))
+            bs = context.newString(t, style=style)
+            newTextBox(bs, parent=page, fill=(0.5, 0.5, 0.5, 0.5), conditions=conditions)
 
-page.solve()
+    page.solve()
+    doc.export('_export/WordAlignment-%s.pdf' % context.name)
 
-doc.export('_export/WordAlignment.pdf')
+if __name__ == '__main__':
+
+
+    from pagebot import getContext
+
+    for contextName in ('DrawBot', 'Flat'):
+        context = getContext(contextName)
+        wordAlignment(context)
