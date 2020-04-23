4b769fb8c0ae0bacad66d98496dfcbfe3bd6ea49
Michiel <michiel@kauwatjoe.com>
2020-04-23 12:43:59 +0200

    Testing if Sketch context is loaded.

---
+++ Advanced/Testing/Sketch.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     Strings.py
+#
+#     Tests pagebot string and style classes in different contexts.
+
+from pagebot import getContext
+
+
+context = getContext('Sketch')
+print(context)
