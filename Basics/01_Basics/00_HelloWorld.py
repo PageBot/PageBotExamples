a8411dbb53a38ec75fdc423f0c6527d6581d9d79
petrvanblokland <buro@petr.com>
2020-04-23 13:09:19 +0200

    Fixing the example to match the major change for BabelString

--- Basics/01_Basics/00_HelloWorld.py
+++ Basics/01_Basics/00_HelloWorld.py
@@ -26,10 +26,10 @@ def helloWorld(context):
     # First page in the list is uneven (right side)
     # This is different from “normal” Python list, where the index starts at 0.
     page = doc[1]
-    # Create a new rectangle element with (x, y) conditions
-    newText('Hello World', x=30, y=0,
-        font=f, fontSize=140, textFill=0.2, parent=page)
-
+    # Create a new Text element with (x, y) conditions
+    e = newText('Hello World', x=30, y=0, font=f, fontSize=140, textFill=0.2, parent=page)
+    print(e)
+    
     # Export the document page as png, so it shows as web image.
     doc.export('_export/HelloWorld-%s.png' % view.name)
 
