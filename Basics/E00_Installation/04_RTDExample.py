358f7c28767328f36b7b7fb4358af3b434122a3c
Michiel <michiel@kauwatjoe.com>
2020-05-12 09:59:11 +0200

    Doc needs a context.

--- Basics/E00_Installation/04_RTDExample.py
+++ Basics/E00_Installation/04_RTDExample.py
@@ -15,14 +15,16 @@
 #    04_RTDExample.py
 #
 
+from pagebot import getContext
 from pagebot.document import Document
 from pagebot.elements import newRect
 from pagebot.conditions import Center2Center, Middle2Middle
 from pagebot.toolbox.units import pt
 from pagebot.toolbox.color import color
 
+context = getContext()
 W, H = pt(300, 200) # Get size units
-doc = Document(w=W, h=H)
+doc = Document(w=W, h=H, context=context)
 page = doc[1]
 newRect(parent=page, fill=color('red'), size=pt(240, 140), showDimensions=True, conditions=[Center2Center(), Middle2Middle()])
 page.solve()
