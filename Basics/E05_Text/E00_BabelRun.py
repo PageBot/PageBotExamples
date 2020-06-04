ac8ef12c05ce441c4b00e1b9fa9126ebc91877af
Michiel <michiel@kauwatjoe.com>
2020-06-04 16:59:54 +0200

    Printing object values to compare contexts.

--- Basics/E05_Text/E00_BabelRun.py
+++ Basics/E05_Text/E00_BabelRun.py
@@ -12,7 +12,7 @@
 #     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
-#     E00_BabelLine.py
+#     E00_BabelRun.py
 #
 #     For demo, run this in DrawBot, with PageBot installed.
 #
@@ -28,28 +28,30 @@ from pagebot.toolbox.loremipsum import loremipsum
 from pagebot.toolbox.units import pt, em
 
 H, W = A4 # Standard paper size from constants.
-loremIpsum = loremipsum()
 
-def babelLine(contextName):
+def babelRun(contextName):
     context = getContext(contextName)
     context.newPage(W, H) # Make a new A4 page.
 
     # Define font, fontSize and color of the square
-    fontName = 'PageBot-Regular'
-    fontSize = pt(24)
     textColor = color(1, 0, 0)
 
-    # Define the style of the text, alignment is centered on baseline.
-    style = dict(font=fontName, fontSize=fontSize, tracking=-em(0.02),
-            leading=em(1), textFill=0) #, xTextAlign=CENTER)
+    style1 = dict(font='PageBot-Regular', fontSize=pt(20), tracking=-em(0.02),
+            leading=em(1), textFill=0)
+
+    style2 = dict(font='PageBot-Bold', fontSize=pt(12), tracking=-em(0.1),
+            leading=em(1.2), textFill=0.5)
+
 
     # Have the context create a BabelString with the style.
-    w = pt(500)
-    bs = context.newString(bs=loremIpsum, style=style, w=w)
-    #print(bs.h)
+    w = pt(400)
+    h = pt(200)
+    bs = context.newString(bs=10*'ABCD', style=style1, w=w, h=h)
+    bs2 = context.newString(bs=10*'EFGH', style=style2, w=w, h=h)
+    bs += bs2
 
-    x = 0
-    y = H / 2
+    x = 0 # Left side of the frame
+    y = H / 2 # Bottom position
 
     context.drawString(bs, (x, y))
     r = 2
@@ -59,14 +61,25 @@ def babelLine(contextName):
     context.marker(x, y, r=r, fontSize=pt(10))
     context.fill(None)
     context.stroke((0, 1, ))
-    context.rect(x, y, bs.tw, bs.th)
+    context.rect(x, y + bs.h - bs.th, bs.tw, bs.th)
     context.stroke((1, 0, ))
     context.rect(x, y, bs.w, bs.h)
 
-    for i, line in enumerate(bs.lines):
-        context.marker(x, y-line.y, r=r, fontSize=pt(6), prefix='#%d' % i)
 
-    context.saveImage('_export/00_BabelLine-%s.pdf' % contextName)
+    print('\n')
+    print(' * ', type(bs))
+    print(bs)
+    print(bs.cs)
+
+    for run in bs.runs:
+        print(run, run.style, run._cr)
+
+    for i, line in enumerate(bs.lines):
+        print(line, line.cLine)
+        context.marker(x + w, y-line.y, r=r, fontSize=pt(6), prefix='#%d' % i)
+
+
+    context.saveImage('_export/00_BabelRun-%s.pdf' % contextName)
 
 for contextName in ('DrawBot', 'Flat'):
-    babelLine(contextName)
+    babelRun(contextName)
