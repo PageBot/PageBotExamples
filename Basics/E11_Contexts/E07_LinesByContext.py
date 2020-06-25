4e6eb6e38798c47c8d3f79c34b8d385f757d976d
Michiel <michiel@kauwatjoe.com>
2020-06-25 16:16:19 +0200

    Testing sizes.

--- Basics/E11_Contexts/E07_LinesByContext.py
+++ Basics/E11_Contexts/E07_LinesByContext.py
@@ -42,47 +42,39 @@ for contextName in ('DrawBot', 'Flat'):
     page = doc[1] # Get the one and single page of the document.
     page.padding = P, P, 2*P, P # Set the page padding, not equal to test vertical position.
 
-    style = dict(font='PageBot-Regular', fontSize=100, textFill=color(0))
-    bs = context.newString('ABCD', style)
-    bs.add('EFGH\\n', dict(fontSize=200))
+    style = dict(font='PageBot-Regular', fontSize=100, textFill=color(0), w=800)
+    bs = context.newString('ABCD', style, w=800)
+    print(bs.w)
+    #print(bs.topLineXHeight)
+    bs.add('EFGH', dict(fontSize=200, textFill=color(0, 1, 0)))
+    print(bs.w)
+    #print(bs.topLineXHeight)
     bs.add('IJKL', dict(fontSize=300))
+    #print(bs.topLineXHeight)
+    #print(bs.bottomLineDescender)
+    #print(bs.bottomLineDescender_p)
+
     # Parent of the element is the current page.
-    e = newText(bs, w=SQ, h=SQ, parent=page, conditions=Fit(),
-        fill=(0.8), stroke=noColor)
-    print(e.bs) # <-- $ABCD$
-    print(e.bs.cs) # FormattedString (DrawBot), <FlatBabelData (Flat)
+    e = newText(bs, w=SQ, h=SQ, parent=page, conditions=Fit())
+    print(bs.w)
+    x, y, w, h = e.x, e.y, e.w, e.h
+    newRect(parent=page, w=w, h=h, x=x, y=y, fill=None, stroke=color(0, 0, 1),
+            strokeWidth=0.5)
+
+
+
+    #print(e.bs)
+    #print(e.bs.cs) # FormattedString (DrawBot), <FlatBabelData (Flat)
+
+    for line in bs.lines:
+        for run in line.runs:
+            print(' * run', run)
 
     # Solve conditions of all placed elements on the page
     page.solve()
 
-    '''
-    if contextName == 'Flat':
-        span = bs.cs.txt.paragraphs[0].spans[0]
-        print('e.bs.cs.txt.paragraphs[0].spans[0].string', span.string, span.style.width(span.string))
-        print('e.bs.cs.pt.width, e.bs.cs.pt.height', e.bs.cs.pt.width, e.bs.cs.pt.height)
-        for height, run in e.bs.cs.pt.layout.runs():
-            for rr in run:
-                print('e.bs.cs.pt.layout.runs():', rr)
-        print('e.bs.textSize:', e.bs.textSize)
-        print('e.bs.getTextSize():', e.bs.getTextSize())
-        print('e.bs.getTextSize(w=e.w, h=e.h):', e.bs.getTextSize(w=e.w, h=e.h))
-
-        print('context.textSize(bs):', context.textSize(bs))
-        print('context.textSize(bs, w=e.w, h=e.h):', context.textSize(bs, w=e.w, h=e.h))
-
-
-        span = bs.cs.txt.paragraphs[0].spans[0]
-        newRect(parent=page, w=span.style.width(span.string), h=bs.th, x=page.pl,
-            y=page.ph-page.pt, yAlign=TOP,
-            fill=None, stroke=color(1, 0, 0), strokeWidth=0.5)
-    '''
-
-    newRect(parent=page, w=bs.tw, h=bs.th, x=page.pl, y=page.ph-page.pt, yAlign=TOP,
-        fill=None, stroke=color(0, 0, 1), strokeWidth=0.5)
-
-    # Set some viewing parameters.
     view = doc.view
-    view.showPadding = True # Show the padding of the page, where conditions align.
+    #view.showPadding = True
 
     # Export in _export folder that does not commit in Git. Force to export PDF.
     doc.export(FILE_NAME)
