2ae11fd9ff9af945904bb28ca7cbe81391c6654e
Michiel <michiel@kauwatjoe.com>
2020-06-29 13:50:25 +0200

    Testing BabelString addition.

--- Basics/E06_Fonts/E00_FontContent.py
+++ Basics/E06_Fonts/E00_FontContent.py
@@ -25,76 +25,81 @@ from pagebot.fonttoolbox.objects.font import findFont
 from pagebot.toolbox.color import color, noColor
 from pagebot.toolbox.units import pt
 
-c = getContext()
 
-EXPORT_PATH = '_export/E00_FontContent.pdf'
+def draw(contextName):
+    c = getContext(contextName)
 
-L = 50
-W = H = 1000
-GX = GY = 11
-M = 50
+    EXPORT_PATH = '_export/E00_FontContent-%s.pdf' % contextName
 
-FONT_NAME = 'PageBot-Regular'
-#FONT_NAME = 'Amstelvar-Roman-VF'
+    L = 50
+    W = H = 1000
+    GX = GY = 11
+    M = 50
 
-f = findFont(FONT_NAME) # Get PageBot Font instance of Variable font.
+    FONT_NAME = 'PageBot-Regular'
+    #FONT_NAME = 'Amstelvar-Roman-VF'
 
-if f is None:
-    print('%s cannot be found' % FONT_NAME)
-else:
-    # Names and text fields
-    print('-- Names', '-'*L)
-    print('Full name:', f.info.fullName)
-    print('Family name:', f.info.familyName)
-    print('Style name:', f.info.styleName)
-    print('PS name:', f.info.psName)
-    print('Designer:', f.info.designer)
-    print('Description:', f.info.description)
-    print('Trademark:', f.info.trademark)
-    #(Metrics)
-    print('-- Metrics', '-'*L)
-    print('Ascender:', f.info.ascender)
-    print('Typo Ascender:', f.info.typoAscender)
-    print('capHeight:', f.info.capHeight)
-    print('xHeight:', f.info.xHeight)
-    print('Descender:', f.info.descender)
-    print('Typo Descender:', f.info.typoDescender)
-    print('Line gap:', f.info.lineGap)
-    print('superscriptXSize:', f.info.superscriptXSize)
-    print('OS/2 Width class:', f.info.widthClass)
-    print('OS/2 Weight class:', f.info.weightClass)
-    print('subscriptXOffset:', f.info.subscriptXOffset)
-    print('strikeoutPosition:', f.info.strikeoutPosition)
-    print('SubscriptXSize:', f.info.subscriptXSize)
-    print('superscriptYOffset:', f.info.superscriptYOffset)
-    print('strikeoutSize:', f.info.strikeoutSize)
-    print('subscriptYSize:', f.info.subscriptYSize)
-    print('superscriptYSize:', f.info.superscriptYSize)
-    print('italicAngle:', f.info.italicAngle)
-    print('unitsPerEm:', f.info.unitsPerEm)
-    #(GPOS)
-    print('-- GPOS', '-'*L)
-    print('gposFeatures:', f.info.gposFeatures)
-    #(GSUB)
-    print('-- GSUB', '-'*L)
-    print('gsubFeatures:', f.info.gsubFeatures)
-    # Glyph content of the font
-    print('-- Glyhps sorted by name', '-'*L)
-    #print('Char set:', f.info.charSet)
-    print('Glyph set:', f.info.glyphSet)
+    f = findFont(FONT_NAME) # Get PageBot Font instance of Variable font.
 
-    glyphIndex = 1
+    if f is None:
+        print('%s cannot be found' % FONT_NAME)
+    else:
+        # Names and text fields
+        print('-- Names', '-'*L)
+        print('Full name:', f.info.fullName)
+        print('Family name:', f.info.familyName)
+        print('Style name:', f.info.styleName)
+        print('PS name:', f.info.psName)
+        print('Designer:', f.info.designer)
+        print('Description:', f.info.description)
+        print('Trademark:', f.info.trademark)
+        #(Metrics)
+        print('-- Metrics', '-'*L)
+        print('Ascender:', f.info.ascender)
+        print('Typo Ascender:', f.info.typoAscender)
+        print('capHeight:', f.info.capHeight)
+        print('xHeight:', f.info.xHeight)
+        print('Descender:', f.info.descender)
+        print('Typo Descender:', f.info.typoDescender)
+        print('Line gap:', f.info.lineGap)
+        print('superscriptXSize:', f.info.superscriptXSize)
+        print('OS/2 Width class:', f.info.widthClass)
+        print('OS/2 Weight class:', f.info.weightClass)
+        print('subscriptXOffset:', f.info.subscriptXOffset)
+        print('strikeoutPosition:', f.info.strikeoutPosition)
+        print('SubscriptXSize:', f.info.subscriptXSize)
+        print('superscriptYOffset:', f.info.superscriptYOffset)
+        print('strikeoutSize:', f.info.strikeoutSize)
+        print('subscriptYSize:', f.info.subscriptYSize)
+        print('superscriptYSize:', f.info.superscriptYSize)
+        print('italicAngle:', f.info.italicAngle)
+        print('unitsPerEm:', f.info.unitsPerEm)
+        #(GPOS)
+        print('-- GPOS', '-'*L)
+        print('gposFeatures:', f.info.gposFeatures)
+        #(GSUB)
+        print('-- GSUB', '-'*L)
+        print('gsubFeatures:', f.info.gsubFeatures)
+        # Glyph content of the font
+        print('-- Glyhps sorted by name', '-'*L)
+        #print('Char set:', f.info.charSet)
+        print('Glyph set:', f.info.glyphSet)
 
-    for yIndex in range(GY):
-        for xIndex in range(GX):
-            # Offset of drawing origin
-            if glyphIndex > len(f.info.glyphSet):
-                break # Just do one page for sample demo.
-            c.save()
-            c.translate(M+(W-2*M)/GX*xIndex, H-M-(H-2*M)/GY*(yIndex+1))
-            g = f[f.info.glyphSet[glyphIndex-1]]
-            c.drawGlyph(g, x=0, y=0, fontSize=60, fill=color(0.1), stroke=noColor)
-            glyphIndex += 1
-            c.restore()
+        glyphIndex = 1
 
-    c.saveImage(EXPORT_PATH)
+        for yIndex in range(GY):
+            for xIndex in range(GX):
+                # Offset of drawing origin
+                if glyphIndex > len(f.info.glyphSet):
+                    break # Just do one page for sample demo.
+                c.save()
+                c.translate(M+(W-2*M)/GX*xIndex, H-M-(H-2*M)/GY*(yIndex+1))
+                g = f[f.info.glyphSet[glyphIndex-1]]
+                c.drawGlyph(g, x=0, y=0, fontSize=60, fill=color(0.1), stroke=noColor)
+                glyphIndex += 1
+                c.restore()
+
+        c.saveImage(EXPORT_PATH)
+
+for contextName in ('DrawBot', 'Flat'):
+    draw(contextName)
--- Basics/E06_Fonts/E01_FontMetrics.py
+++ Basics/E06_Fonts/E01_FontMetrics.py
@@ -12,50 +12,68 @@
 #     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
-#     FontMetrics.py
+#     E01_FontMetrics.py
 #
-#     It is possible to measure the metrics from a font.
-#     These show examples how to do that.
-#
+
 from pagebot.document import Document
-from pagebot.elements import *
+from pagebot.elements import newLine, newText
 from pagebot import getContext
 from pagebot.toolbox.units import pt, em
 from pagebot.toolbox.color import Color, color
 from pagebot.fonttoolbox.objects.font import findFont
 
-context = getContext('DrawBot')
+def draw(contextName):
+    context = getContext(contextName)
+    font = findFont('PageBot-Regular')
+    W, H = pt(800, 200)
+    fontSize = pt(100)
+    print('Size:', fontSize)
+    txt = "Hello World"
+    x, y = pt(50, 50) # Position of the text block and relative position of the lines.
+    #context.marker(x, y)
 
-W, H = pt(1000, 200)
-fontSize = pt(100)
+    # Get the font for metrics.
+    ratio = fontSize / font.info.unitsPerEm
+    print('Scaling ratio', ratio)
+    style = dict(fontSize=fontSize, leading=em(1), font=font)
+    doc = Document(w=W, h=H, context=context)
+    page = doc[1]
 
-txt = "Hello World"
-x, y = pt(50, 50) # Position of the text block and relative position of the lines.
+    # draw the text.
+    bs = context.newString(txt, style=style)
+    tw, th = bs.textSize
+    print('Text width & height', tw, th)
+    e = newText(bs, x=x, y=y, parent=page)
 
-# Get the font for metrics.
-font = findFont('PageBot-Regular')
-ratio = fontSize/font.info.unitsPerEm
-style = dict(fontSize=fontSize, leading=em(1), font=font)
+    # calculate the size of the text.
+    textWidth, textHeight = bs.textSize
 
-doc = Document(w=W, h=H, context=context)
-page = doc[1]
+    # set a red stroke color.
+    red = color(1, 0, 0) # Color instance can translate into any other color type.
+    green = color(0, 1, 0) # Color instance can translate into any other color type.
 
-# draw the text.
-bs = context.newString(txt, style=style)
-tw, th = bs.textSize
-e = newText(bs, x=x, y=y, w=800, h=200, parent=page)
+    # loop over all font metrics and scale to the fontSize
 
-# calculate the size of the text.
-textWidth, textHeight = bs.textSize
 
-# set a red stroke color
-strokeColor = color(1, 0, 0) # Color instance can translate into any other color type.
+    d = dict(baseline=0, descenter=font.getDescender(),
+            ascender=font.getAscender(), xheight=font.getXHeight(),
+            capheight=font.getCapHeight())
 
-# loop over all font metrics and scale to the fontSize
+    padding=pt(10)
+    style = dict(fontSize=pt(10), font=font)
 
-for metric in (0, font.getDescender(), font.getAscender(), font.getXHeight(), font.getCapHeight()):
-    # Draw a red line with the size of the drawn text
-    # Context drawing functions expect measures to be Unit instances.
-    newLine(x=x, y=y+metric*ratio, w=tw, h=0, parent=page, strokeColor=strokeColor)
+    for name, metric in d.items():
+        # Draw a red line with the size of the drawn text
+        # Context drawing functions expect measures to be Unit instances.
+        value = metric * ratio
+        #print(value)
+        newLine(x=x, y=y+value, w=tw, h=0, parent=page, stroke=green, strokeWidth=0.5)
+        text = '%s: %s / %s' % (name, metric, value)
+        t = context.newString(text, style=style)
+        newText(t, x=x+textWidth+padding, y=y+value, parent=page)
 
-doc.export('_export/E00_FontMetric.pdf')
+    doc.export('_export/E00_FontMetric-%s.pdf' % contextName)
+
+
+for contextName in ('DrawBot', 'Flat'):
+    draw(contextName)
