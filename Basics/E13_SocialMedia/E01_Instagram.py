968ddd1d7c7f3d463587cac02665ba4c3ffa4b65
petrvanblokland <buro@petr.com>
2020-08-04 00:46:09 +0200

    Adding Instagram example (not finished, working version is in the DesignDesign.Space site)

--- Basics/E13_SocialMedia/E01_Instagram.py
+++ Basics/E13_SocialMedia/E01_Instagram.py
@@ -20,9 +20,9 @@
 from pagebot import getContext
 from pagebot.filepaths import getResourcesPath
 from pagebot.publications.instagram import InstagramPost
-from pagebot.toolbox.units import px, em
+from pagebot.toolbox.units import px, em, pt
 from pagebot.toolbox.color import color, noColor
-from pagebot.constants import InstagramSquare, MAX_IMAGE_WIDTH
+from pagebot.constants import InstagramSquare, CinemaWide, MAX_IMAGE_WIDTH
 from pagebot.typesetter import Typesetter
 
 context = getContext()
@@ -36,16 +36,18 @@ import flat
 
 # Example image that has nice areas to put text as example.
 imagePath = getResourcesPath() + '/images/pagebot.png'
-EXPORT_PATH = '_export/05_ImageClipping-%s.pdf' % context.name
+EXPORT_PATH = '_export/01_Instagram-%s.pdf' % context.name
 MD_PATHS = ['DDS-home.md']
 
-h2Style = dict(font='Upgrade-Medium', fontSize=px(120), leading=em(1), textFill=color(1))
-h3Style = dict(font='Upgrade-Regular', fontSize=px(90), leading=em(1), textFill=color(1))
-h4Style = dict(font='Upgrade-Semibold', fontSize=px(90), leading=em(1), textFill=color(1))
+h2Style = dict(font='Upgrade-Medium', fontSize=pt(80), leading=em(1), textFill=color(1))
+h3Style = dict(font='Upgrade-Regular', fontSize=pt(40), leading=em(1), textFill=color(1))
+h4Style = dict(font='Upgrade-Semibold', fontSize=pt(40), leading=em(1), textFill=color(1))
+supStyle = dict(font='Upgrade-Semibold', fontSize=pt(40), leading=em(1), textFill=color(1,0,0))
 styles = dict(
     h2=h2Style,
     h3=h3Style,
     h4=h4Style,
+    sub=supStyle,
 )
 instagram = InstagramPost(styles=styles)
 w, h = InstagramSquare
@@ -75,7 +77,7 @@ for e in t.galley.elements:
             elif run.style.get('name') == 'h4':
                 bs4 += context.newString(run.s.strip(), h4Style)
             elif run.style.get('name') == 'sup':
-                bs4 += context.newString(run.s.strip(), h4Style)
+                bs4 += context.newString(run.s.strip(), supStyle) + ' '
     elif e.isImage:
         if not None in (bs2, bs3, bs4):            
             bannerData.append((bs2, bs3, bs4, e.path, e.alt))
@@ -84,8 +86,11 @@ for e in t.galley.elements:
 
 # Now we have all content, we can create the pages, one per post
 page = doc[1]
+# For now direct output, as long as vertical position of text is not accurate
 for bs2, bs3, bs4, imagePath, alt in bannerData:
-    #print(bs2, bs3, bs4, imagePath)
+    context.newPage(page.w, page.h)
+    bs4.s = bs4.s.split('•')[0].replace('th', ', ')
+
     iw, ih = context.imageSize(imagePath)
     if iw > ih:
         iw, ih = None, h
@@ -97,6 +102,10 @@ for bs2, bs3, bs4, imagePath, alt in bannerData:
     else:
         xAlign = None
         x = 0
+    sc = h/ih
+    x = y
+    context.size(sc)
+    context.image(imagePath, x)
     newImage(path=imagePath, parent=page, x=x, y=0, w=iw, h=ih, xAlign=xAlign)
 
     if bs2 is not None:
@@ -117,89 +126,3 @@ for bs2, bs3, bs4, imagePath, alt in bannerData:
     page = page.next
 
 
-
-def imageClipping(contextName):
-    context = getContext(contextName)
-
-    # Example image that has nice areas to put text as example.
-    imagePath = getResourcesPath() + '/images/pagebot.png'
-    EXPORT_PATH = '_export/05_ImageClipping-%s.pdf' % contextName
-
-    W = pt(400) # Document size
-    H = pt(400)
-    PADDING = pt(24) # Page padding on all sides
-    BLEED = pt(6)
-
-    # Create a new document with 1 page. Set overall size and padding.
-    doc = Document(w=W, h=H, title=EXPORT_PATH, padding=PADDING, context=context)
-    # Get the default page view of the document and set viewing parameters
-    view = doc.view
-    view.padding = pt(30)
-    view.showFrame = True
-    view.showPadding = True
-    view.showColorBars = False
-    view.showCropMarks = True
-    view.showRegistrationMarks = True
-    view.showNameInfo = True # Showing page info and title on top of the page.
-
-    # Get the page
-    page = doc[1]
-    # Make image box as child element of the page and set its layout conditions.
-    # The image is portrait, so fitting vertical makes the image fit in the
-    # padding box of the page.
-    conditions = [Fit2Height(), Center2Center()]
-    im = newImage(imagePath, parent=page, conditions=conditions,
-            showOrigin=True)
-
-    page = page.next
-    # Fitting the image by width, it does not fit vertically anymore.
-    conditions = [Fit2Width(), Middle2Middle()]
-    im = newImage(imagePath, parent=page, conditions=conditions,
-            showOrigin=True)
-
-    page = page.next
-    # Fitting the image by width, it does not fit vertically anymore.
-    # Adding a mask as sibling, we can clip the image on the page padding.
-    conditions = [Fit2Width(), Middle2Middle()]
-    im = newImage(imagePath, parent=page, conditions=conditions,
-            showOrigin=True)
-    conditions =[Fit()]
-    mask = newMask(parent=page, conditions=conditions, # Fit page padding
-            showOrigin=True)
-
-    page = page.next
-    # Fitting the image by width, it does not fit vertically anymore.
-    # The Mask can be any size and position on the page.
-    conditions = [Fit2Width(), Middle2Middle()]
-    im = newImage(imagePath, parent=page, conditions=conditions,
-            showOrigin=True)
-    conditions = [Right2Right(), Top2Top()]
-    mask = newMask(parent=page, conditions=conditions,
-            w=page.pw/2, h=page.ph/2, showOrigin=True)
-
-    page = page.next
-    page.bleed = BLEED # Set all bleed sides to the same value
-    # Fitting the image by width, it does not fit vertically anymore.
-    # Making the image bleed on page width.
-    conditions = [Left2BleedLeft(), Fit2BleedRight(), Middle2Middle()]
-    im = newImage(imagePath, parent=page, conditions=conditions,
-            showOrigin=True)
-    conditions = [Right2Right(), Top2Top()]
-
-    page = page.next
-    page.bleed = BLEED # Set all bleed sides to the same value
-    # Fitting the image by width, it does not fit vertically anymore.
-    # Making the image bleed on page width.
-    # Now the mask needs to follow the bleed fit too.
-    conditions = [Left2BleedLeft(), Fit2BleedRight(), Middle2Middle()]
-    im = newImage(imagePath, parent=page, conditions=conditions,
-            showOrigin=True)
-    # Fit the mask on top half of the page, including bleed
-    conditions = [Left2BleedLeft(), Fit2BleedRight(), Top2BleedTop()]
-    mask = newMask(parent=page, conditions=conditions,
-            w=page.w/2+2*BLEED, h=page.h/2+BLEED, showOrigin=True)
-
-    doc.solve()
-
-    # Export the document to this PDF file.
-    doc.export(EXPORT_PATH)
