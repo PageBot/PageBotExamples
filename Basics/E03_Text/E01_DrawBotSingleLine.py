1347d62cb33f3cf154b650782dfad8214275f29a
Michiel <michiel@kauwatjoe.com>
2020-09-17 10:45:04 +0200

    Drawing text on baseline.

--- Basics/E03_Text/E01_DrawBotSingleLine.py
+++ Basics/E03_Text/E01_DrawBotSingleLine.py
@@ -33,6 +33,9 @@ loremIpsum = loremipsum()
 W = 800
 H = 800
 P = 10
+TOPBASELINE = 30*P
+FONTSIZE = 200
+LEADING = 1.3
 
 def drawWord(context, x, y, word, fontSize, leading):
     style = dict(font=fontName, fontSize=fontSize, leading=em(leading))
@@ -46,7 +49,7 @@ def drawWord(context, x, y, word, fontSize, leading):
     ctBox = CTFramesetterCreateFrame(setter, (0, 0), path, None)
     ctLines = CTFrameGetLines(ctBox)
     origins = CTFrameGetLineOrigins(ctBox, (0, len(ctLines)), None)
-    context.drawText(bs, (x, y, W, H))
+    #context.drawText(bs, (x, y, W, H))
     context.drawString(bs, (x, y))
 
     for i, origin in enumerate(origins):
@@ -57,19 +60,22 @@ def drawWord(context, x, y, word, fontSize, leading):
         p2 = (x1, yLine)
         context.stroke((1, 0, 0))
         context.line(p1, p2)
-        context.marker(x1, yLine, r=r, fontSize=pt(5), prefix='# %s = %dpt' % (i, origin.y))
+        yFromTop = H - yLine - y
+        context.marker(x1, yLine, r=r, fontSize=pt(5), prefix='# %s = %dpt from top' % (i, yFromTop))
 
     # Total text height.
     context.stroke((0, 0, 1))
     x0 = x + bs.tw + P
-    y0 = H - y
-    y1 = y0 - bs.th
-    y2 = y1 + (y0 - y1) / 2
+    y1 = y + bs.topLineDescender
+    y0 = y1 + bs.th
+    y2 = y0 + (y1 - y0) / 2
 
     p0 = (x0, y0)
     p1 = (x0, y1)
     p2 = (x0 + P/2, y2)
     context.line(p0, p1)
+    context.marker(x0, y0, prefix='p0: (%d, %d)' % p0)
+    context.marker(x0, y1, prefix='p1: (%d, %d)' % p1)
     context.text('bs.th = %dpt' % bs.th, p2)
 
     x0 = x
@@ -92,11 +98,15 @@ def draw(fontName, fontSize, leading):
     context = getContext('DrawBot')
     context.newDrawing()
     context.newPage(W, H)
-    drawWord(context, P, P, 'Hpxk', fontSize, leading)
+    drawWord(context, P, H-TOPBASELINE, 'Hp', fontSize, leading)
+    drawWord(context, W/2, H-TOPBASELINE, 'xk', fontSize, leading)
+    context.stroke((0, 0, 1))
+    p0, p1 = (0, H-TOPBASELINE), (W, H-TOPBASELINE)
+    context.line(p0, p1)
     context.saveImage(exportPath)
     context.clear()
 
 for fontName in ('Roboto-Regular','PageBot-Regular', 'Bungee-Regular'):
-    fontSize=200
-    leading=1.3
+    fontSize=FONTSIZE
+    leading=LEADING
     draw(fontName, fontSize, leading)
