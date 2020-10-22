df077a18c7b37f4a3581d0e55b877cb49b4d2f67
Michiel <michiel@kauwatjoe.com>
2020-10-22 13:44:02 +0200

    Slight rename.

--- Basics/E01_Basics/E06_BasicElements.py
+++ Basics/E01_Basics/E06_BasicElements.py
@@ -12,7 +12,7 @@
 #     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
-#     E06_Elements.py
+#     E06_BasicElements.py
 #
 #     Test handling of pages in a document.
 #
