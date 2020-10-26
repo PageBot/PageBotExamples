377771a961e297831663f73b7678e5352e01be5a
Michiel <michiel@kauwatjoe.com>
2020-10-26 11:43:20 +0100

    E03 gives correct results, E02 still needs some debugging in the Flat context.

--- Basics/E06_Images/E03_ImageAlignment.py
+++ Basics/E06_Images/E03_ImageAlignment.py
@@ -11,166 +11,169 @@
 #     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
-#     E02_ImageAlignment.py
+#     E03_ImageAlignment.py
 #
 #     Draw images aligned on various positions
 #
 from random import random
-from pagebot import getContext
 
-from pagebot.filepaths import getResourcesPath
-from pagebot.document import Document
-from pagebot.toolbox.color import color, noColor
-from pagebot.toolbox.units import em, p, pt, inch, degrees
+from pagebot import getContext
 from pagebot.conditions import * # Import all conditions for convenience.
 from pagebot.constants import *
+from pagebot.document import Document
 from pagebot.elements import *
+from pagebot.filepaths import getResourcesPath
+from pagebot.toolbox.color import color, noColor
+from pagebot.toolbox.units import em, p, pt, inch, degrees
+from pagebot.toolbox.transformer import path2FileName
 
-#context = FlatContext()
-context = getContext('DrawBot')
 
 # Example image that has nice areas to put text as example.
-imagePath = getResourcesPath() + '/images/peppertom_lowres_398x530.png'
-EXPORT_PATH = '_export/02_ImageAlignment.pdf'
+IMAGEPATH = getResourcesPath() + '/images/peppertom_lowres_398x530.png'
+FILENAME = path2FileName(__file__)
 BLEED = pt(6)
 
-W = pt(400) # Document size
+# Document size.
+W = pt(400)
 H = pt(400)
-PADDING = pt(24) # Page padding on all sides
 
-# Create a new document with 1 page. Set overall size and padding.
-doc = Document(w=W, h=H, title=EXPORT_PATH, padding=PADDING, context=context)
-# Get the default page view of the document and set viewing parameters
-view = doc.view
-view.padding = pt(20)
-view.showFrame = True
-view.showPadding = True
-view.showColorBars = False
-view.showCropMarks = True
-view.showRegistrationMarks = True
-view.showNameInfo = True # Showing page info and title on top of the page.
+# Page padding.
+PADDING = pt(24)
 
-def addLabel(e, page, s, condition):
-	style = dict(font='PageBot-Regular', fontSize=pt(16),
-		leading=em(1.2), textFill=0.6)
-	bs = context.newString(s, style)
-	newText(bs, parent=page, w=page.pw, xAlign=LEFT, yAlign=TOP,
-		conditions=[Left2Left(), condition])
+def draw(contextName):
+    context = getContext(contextName)
+    exportPath = '%s/%s-%s.pdf' % (EXPORT, FILENAME, contextName)
+    doc = Document(w=W, h=H, title=exportPath, padding=PADDING, context=context)
+    view = doc.view
+    view.padding = pt(20)
+    view.showFrame = True
+    view.showPadding = True
+    view.showColorBars = False
+    view.showCropMarks = True
+    view.showRegistrationMarks = True
+    view.showNameInfo = True # Showing page info and title on top of the page.
 
-# Get the first page
-# LEFT
-page = doc[1]
-page.bleed = BLEED
-im = newImage(imagePath, parent=page, x=-page.bleedLeft, y=page.h/2,
-	w=page.w/2+page.bleedLeft, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=-page.bleedLeft; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    def addLabel(e, page, s, condition):
+            style = dict(font='PageBot-Regular', fontSize=pt(16),
+                    leading=em(1.2), textFill=0.6)
+            bs = context.newString(s, style)
+            newText(bs, parent=page, w=page.pw, xAlign=LEFT, yAlign=TOP,
+                    conditions=[Left2Left(), condition])
 
-page = page.next
-im = newImage(imagePath, parent=page, x=0, y=page.h/2,
-	w=page.w/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=0; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    # Get the first page
+    # LEFT
+    page = doc[1]
+    page.bleed = BLEED
+    im = newImage(IMAGEPATH, parent=page, x=-page.bleedLeft, y=page.h/2,
+            w=page.w/2+page.bleedLeft, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=-page.bleedLeft; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.pl, y=page.h/2,
-	w=page.pw/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.pl; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=0, y=page.h/2,
+            w=page.w/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=0; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.pl+page.pw/4, y=page.h/2,
-	w=page.pw/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.pl+page.pw/4; y=page.h/2\nxAlign=CENTER; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.pl, y=page.h/2,
+            w=page.pw/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.pl; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	w=page.pw/2, xAlign=RIGHT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.pl+page.pw/4, y=page.h/2,
+            w=page.pw/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.pl+page.pw/4; y=page.h/2\nxAlign=CENTER; yAlign=MIDDLE',
+            Top2Top())
 
-# CENTER
-page = page.next
-im = newImage(imagePath, parent=page, x=page.pl+page.pw/4, y=page.h/2,
-	w=page.pw/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.pl+page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            w=page.pw/2, xAlign=RIGHT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	w=page.pw/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=CENTER; yAlign=MIDDLE',
-	Top2Top())
+    # CENTER
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.pl+page.pw/4, y=page.h/2,
+            w=page.pw/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.pl+page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.pl+page.pw*3/4, y=page.h/2,
-	w=page.pw/2, xAlign=RIGHT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.pl+page.pw*3/4; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            w=page.pw/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=CENTER; yAlign=MIDDLE',
+            Top2Top())
 
-# RIGHT
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	w=page.pw/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.pl+page.pw*3/4, y=page.h/2,
+            w=page.pw/2, xAlign=RIGHT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.pl+page.pw*3/4; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.pl+page.pw*3/4, y=page.h/2,
-	w=page.pw/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.pl*page.pw*3/4; y=page.h/2\nxAlign=CENTER; yAlign=MIDDLE',
-	Top2Top())
+    # RIGHT
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            w=page.pw/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w-page.pr, y=page.h/2,
-	w=page.pw/2, xAlign=RIGHT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w-page.pr; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.pl+page.pw*3/4, y=page.h/2,
+            w=page.pw/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.pl*page.pw*3/4; y=page.h/2\nxAlign=CENTER; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	w=page.w/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w-page.pr, y=page.h/2,
+            w=page.pw/2, xAlign=RIGHT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w-page.pr; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-page.bleed = BLEED
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	w=page.w/2+page.bleedRight, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            w=page.w/2, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-# TOP
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	h=page.ph/2, xAlign=CENTER, yAlign=BOTTOM, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
-	Bottom2Bottom())
+    page = page.next
+    page.bleed = BLEED
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            w=page.w/2+page.bleedRight, xAlign=LEFT, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-# MIDDLE
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	h=page.ph/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
-	Top2Top())
+    # TOP
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            h=page.ph/2, xAlign=CENTER, yAlign=BOTTOM, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
+            Bottom2Bottom())
 
-# BOTTOM
-page = page.next
-im = newImage(imagePath, parent=page, x=page.w/2, y=page.h/2,
-	h=page.ph/2, xAlign=CENTER, yAlign=TOP, showOrigin=True)
-addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
-	Top2Top())
+    # MIDDLE
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            h=page.ph/2, xAlign=CENTER, yAlign=MIDDLE, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
+            Top2Top())
 
-page = page.next
-im = newRect(fill=(1, 0, 0), parent=page, y=page.h/2,
-	conditions=(Fit2Width(), Bottom2Bottom()),
-	h=page.ph/2, yAlign=TOP, showOrigin=True)
-addLabel(im, page, 'x=Fit2Width(); y=Bottom2Bottom()\nxAlign=LEFT; yAlign=MIDDLE',
-	Top2Top())
+    # BOTTOM
+    page = page.next
+    im = newImage(IMAGEPATH, parent=page, x=page.w/2, y=page.h/2,
+            h=page.ph/2, xAlign=CENTER, yAlign=TOP, showOrigin=True)
+    addLabel(im, page, 'x=page.w/2; y=page.h/2\nxAlign=RIGHT; yAlign=MIDDLE',
+            Top2Top())
 
-doc.solve()
+    page = page.next
+    im = newRect(fill=(1, 0, 0), parent=page, y=page.h/2,
+            conditions=(Fit2Width(), Bottom2Bottom()),
+            h=page.ph/2, yAlign=TOP, showOrigin=True)
+    addLabel(im, page, 'x=Fit2Width(); y=Bottom2Bottom()\nxAlign=LEFT; yAlign=MIDDLE',
+            Top2Top())
 
-# Export the document to this PDF file.
-doc.export(EXPORT_PATH)
+    doc.solve()
+    doc.export(exportPath)
 
+for contextName in ('DrawBot', 'Flat'):
+    draw(contextName)
