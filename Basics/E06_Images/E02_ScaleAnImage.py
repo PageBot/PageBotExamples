af32914bc4af0bd6afec1bd6c7630a20eef6dba5
Michiel <michiel@kauwatjoe.com>
2020-10-29 14:58:59 +0100

    Cleaning up script.

--- Basics/E06_Images/E02_ScaleAnImage.py
+++ Basics/E06_Images/E02_ScaleAnImage.py
@@ -29,19 +29,16 @@ def draw(contextName):
     path = getResourcesPath() + "/images/%s.jpg" % IMAGENAME
     w, h = context.imageSize(path)
 
-    # Let's say we want to scale it to 50%. The 0.5 is the multiplication
-    # factor.
     newScale = 0.5
 
     # Make a page with the size of the scaled image, rounded to whole pixels.
     context.newPage(pt(int(w*newScale)), pt(int(h*newScale)))
 
-   # Saves the “graphics state“, just in case the script is extended later,
-    # where other operation need to work in 100%.
-    context.save()
-    # Make all drawing scale to 50%
+    # Tells the context to use the new scale.
     context.scale(newScale)
-    # Draw the scaled image at the bottom-left corner. It fills the whole page.
+
+    # Draws the scaled image at the bottom-left corner. It should fill the
+    # entire page.
     context.image(path, pt(0, 0))
 
     # Saves the page as png file (and also do conversion from jpg to png this
@@ -49,16 +46,9 @@ def draw(contextName):
     if not os.path.exists('_export/'):
         os.makedirs('_export/')
 
-    # Note that resulting images may look sharper, but has 4.5x the size of the .jpg.
-    # 944Kb size
-    context.saveImage('_export/%s-%s-%d-%s.png' % (FILENAME, IMAGENAME, newScale*100, contextName))
-    # 168Kb size
-    context.saveImage('_export/%s-%s-%d-%s.jpg' % (FILENAME, IMAGENAME, newScale*100, contextName))
-    # 346Kb size
-    context.saveImage('_export/%s-%s-%d-%s.gif' % (FILENAME, IMAGENAME, newScale*100, contextName))
-
-    # Restores the graphics state, so context scaling is back to 100% after this.
-    context.restore()
+    #context.saveImage('_export/%s-%s-%d-%s.png' % (FILENAME, IMAGENAME, newScale*100, contextName))
+    context.saveImage('_export/%s-%s-%dpercent-%s.jpg' % (FILENAME, IMAGENAME, newScale*100, contextName))
+    #context.saveImage('_export/%s-%s-%d-%s.gif' % (FILENAME, IMAGENAME, newScale*100, contextName))
 
 for contextName in ('DrawBot', 'Flat'):
     draw(contextName)
