485289a33818d6e573dcfb83a2d28cb3bc2adb82
Michiel <michiel@kauwatjoe.com>
2020-11-05 16:06:23 +0100

    Adding more variable elements.

--- Basics/E10_VariableFonts/E00_VariableElements.py
+++ Basics/E10_VariableFonts/E00_VariableElements.py
@@ -12,18 +12,21 @@
 #     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
-#     E00_VariableCircle.py
+#     E00_VariableElements.py
 #
 
 from pagebot import getContext
 from pagebot.constants import EXPORT
+from pagebot.conditions import *
 from pagebot.document import Document
 from pagebot.elements.variablefonts.variablecircle import VariableCircle
+from pagebot.elements.variablefonts.variablecube import VariableCube
+from pagebot.elements.variablefonts.variablescatter import VariableScatter
 from pagebot.fonttoolbox.objects.font import findFont
 from pagebot.toolbox.transformer import path2FileName
 
 FILENAME = path2FileName(__file__)
-W = H = 500
+W = H = 1000
 vfFont = findFont('RobotoDelta_v2-VF')
 
 def draw(contextName):
@@ -31,8 +34,15 @@ def draw(contextName):
     context = getContext(contextName)
     doc = Document(w=W, h=H, context=context)
     page = doc[1]
-    page.padding = 40
-    vc = VariableCircle(vfFont, parent=page, x=40, y=40, w=page.pw)
+    page.padding = 10
+    conditions = (Right2Right(), Float2Top(), Float2Left())
+    options = dict(parent=page, showOrigin=True, showDimensions=True,
+            showElementInfo=True, showFlowConnections=True,
+            conditions=conditions, stroke=0, strokeWidth=0.5, w=400, h=400)
+
+    VariableCircle(vfFont, **options)
+    VariableScatter(vfFont, **options)
+    VariableCube(vfFont, **options)
     doc.export(exportPath)
 
 for contextName in ('DrawBot', 'Flat'):
